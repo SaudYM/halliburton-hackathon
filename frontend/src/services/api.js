8db976071bd86c08cbbce4a7a76320711572b80a
SaudYM <alyamisaud7@gmail.com>
2024-11-27 17:58:48 +0300

    FEb

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000/api", // Adjust base URL as per your backend
+  baseURL: "https://halliburton-hackathon.onrender.com", // Adjust base URL as per your backend
 });
 
 export default api;
